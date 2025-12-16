c3bf52b9d826eb091240ffdebcc1477dd95c9a3e
Kiara <145350450+Kiaraintellipaat@users.noreply.github.com>
2025-12-16 18:44:27 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-prod-state-bucket"
     key            = "infra/terraform.tfstate"
-    region         = "eu-central-1"   # 👈 MUST MATCH BUCKET REGION
+    region         = "us-east-1"   # 👈 MUST MATCH BUCKET REGION
     dynamodb_table = "terraform-lock"
     encrypt        = true
   }
